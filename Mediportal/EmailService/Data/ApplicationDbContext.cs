694a8fcae9cd3c0a27e445291718eb93b1a0d2b0
Karani-Ken <academice79@gmail.com>
2023-11-09 10:09:48 +0300

    started on message service

---
+++ Mediportal/EmailService/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EmailService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailService.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<EmailLoggers> EmailLoggers { get; set; }
+    }
+}
---
+++ Mediportal/EmailService/Models/Dto/EmailMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailService.Models.Dto
+{
+    public class EmailMessage
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}
